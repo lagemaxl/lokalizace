bbcc6a4047c6ee28dcd385b393b8b376ad550869
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2023-03-03 10:21:37 +0100

    format script

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { MapContainer, Marker, TileLayer, useMapEvents, Polyline} from 'react-leaflet';
-import * as L from 'leaflet';
-import './App.css';
+import {
+  MapContainer,
+  Marker,
+  TileLayer,
+  useMapEvents,
+  Polyline,
+} from "react-leaflet";
+import * as L from "leaflet";
+import "./App.css";
 
-
-function Task(props){
-  return(
+function Task(props) {
+  return (
     <div className="task">
       <h2 className="placeTOfind">{props.title}</h2>
       <div className="correct-bar"></div>
     </div>
-  )
+  );
 }
 
 let numberOfLocations = getRandomNumber(0, 76);
 
-function getRandomNumber(min,max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function App() {
@@ -24,19 +29,19 @@ function App() {
   const [position, setPosition] = useState([50.6813617, 14.0078506]);
   const [MouseFree, setMouseFree] = useState(true);
   const [result, setResult] = useState(false);
-  const [resultnum, setResultnum] = useState(0)
+  const [resultnum, setResultnum] = useState(0);
   const [locations, setLocations] = useState([]); //lokace které se budou brát z JSON soubo
   const [isLoading, setIsLoading] = useState(true);
   const [buttonswitch, setButtonswitch] = useState(true);
 
   useEffect(() => {
     fetch("locations.json")
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setLocations(data.locations);
         setIsLoading(false); // označíme, že data byla načtena
       })
-      .catch(error => console.error(error));
+      .catch((error) => console.error(error));
   }, []);
 
   // Zde se vykreslí pouze pokud se data načtou
@@ -44,147 +49,150 @@ function App() {
     return <div>Loading...</div>;
   }
 
-  let rightposition = [locations[numberOfLocations].l1, locations[numberOfLocations].l2];
+  let rightposition = [
+    locations[numberOfLocations].l1,
+    locations[numberOfLocations].l2,
+  ];
   let positions = [position, rightposition];
-
 
   const MapEvents = () => {
     useMapEvents({
       click(e) {
         console.log(e.latlng.lat);
         console.log(e.latlng.lng);
-        if(MouseFree){
+        if (MouseFree) {
           setPosition([e.latlng.lat, e.latlng.lng]);
         }
         positions = [position, rightposition];
       },
     });
     return false;
-}
-    
+  };
 
-    function Zkontrlovatbtn(){
-      setResult(true);
-      setMouseFree(false);
-      const res = Distance(position[0], position[1], rightposition[0], rightposition[1])
-      console.log(res);
-      setResultnum(res.toFixed(1))
-      setButtonswitch(false);
-    }
+  function Zkontrlovatbtn() {
+    setResult(true);
+    setMouseFree(false);
+    const res = Distance(
+      position[0],
+      position[1],
+      rightposition[0],
+      rightposition[1]
+    );
+    console.log(res);
+    setResultnum(res.toFixed(1));
+    setButtonswitch(false);
+  }
 
-    function Next(){
-      numberOfLocations = getRandomNumber(0, 76);
-      setMouseFree(true);
-      setResult(false);
-      setButtonswitch(true);
-      rightposition = [locations[numberOfLocations].l1, locations[numberOfLocations].l2];
-      positions = [position, rightposition];
-    
-    }
+  function Next() {
+    numberOfLocations = getRandomNumber(0, 76);
+    setMouseFree(true);
+    setResult(false);
+    setButtonswitch(true);
+    rightposition = [
+      locations[numberOfLocations].l1,
+      locations[numberOfLocations].l2,
+    ];
+    positions = [position, rightposition];
+  }
 
-    function Result() {
-
-      return(
-        <>
-        <Marker position={rightposition} className="marker"
-                  icon={
-                    new L.Icon({
-                      iconUrl: `https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png`,
-                      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-                      iconSize: [25, 41],
-                      iconAnchor: [12, 41]
-                    })
-                  }
+  function Result() {
+    return (
+      <>
+        <Marker
+          position={rightposition}
+          className="marker"
+          icon={
+            new L.Icon({
+              iconUrl: `https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png`,
+              shadowUrl:
+                "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+            })
+          }
         />
-        <Polyline positions={positions} color={'blue'}/>
-        </>
-      )
-    }
+        <Polyline positions={positions} color={"blue"} />
+      </>
+    );
+  }
 
+  function Distance(lat1, lon1, lat2, lon2) {
+    const earthRadiusKm = 6371;
 
-    function Distance(lat1, lon1, lat2, lon2) {
-      const earthRadiusKm = 6371;
-    
-      const dLat = degreesToRadians(lat2 - lat1);
-      const dLon = degreesToRadians(lon2 - lon1);
-    
-      const lat1Rad = degreesToRadians(lat1);
-      const lat2Rad = degreesToRadians(lat2);
-    
-      const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1Rad) * Math.cos(lat2Rad);
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    
-      return earthRadiusKm * c;
-    }
-    
-    function degreesToRadians(degrees) {
-      return degrees * Math.PI / 180;
-    }
+    const dLat = degreesToRadians(lat2 - lat1);
+    const dLon = degreesToRadians(lon2 - lon1);
 
+    const lat1Rad = degreesToRadians(lat1);
+    const lat2Rad = degreesToRadians(lat2);
+
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.sin(dLon / 2) *
+        Math.sin(dLon / 2) *
+        Math.cos(lat1Rad) *
+        Math.cos(lat2Rad);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return earthRadiusKm * c;
+  }
+
+  function degreesToRadians(degrees) {
+    return (degrees * Math.PI) / 180;
+  }
 
   return (
-  <div className="App">
+    <div className="App">
+      <div className="rightPanel">
+        <h1 className="title">Loklaizace</h1>
+        <h1>Najdi na mapě:</h1>
+        <Task
+          title={locations[numberOfLocations].name}
+          //title = "Null Island"
+        />
+        <h1>Dokončené:</h1>
+        <Task title="Nic" />
+        <Task title="Nic" />
+        <Task title="Nic" />
+        <Task title="Nic" />
 
-    <div className="rightPanel">
+        {buttonswitch ? (
+          <button className="btn-primary" onClick={Zkontrlovatbtn}>
+            Zkontrolovat
+          </button>
+        ) : (
+          <button className="btn-primary" onClick={Next}>
+            Další
+          </button>
+        )}
 
-    <h1 className="title">Loklaizace</h1>
-      <h1>Najdi na mapě:</h1>
-      <Task 
-      title = {locations[numberOfLocations].name}
-      //title = "Null Island"
-      />
-      <h1>Dokončené:</h1>
-      <Task 
-      title = "Nic"
-      />
-      <Task 
-      title = "Nic"
-      />
-      <Task 
-      title = "Nic"
-      />
-      <Task 
-      title = "Nic"
-      />
-
-      {buttonswitch ? <button className="btn-primary" onClick={Zkontrlovatbtn}>Zkontrolovat</button> :  <button className="btn-primary" onClick={Next}>Další</button>}
-
-      <div>
-        <h1 className="title">Vzdálenost: {resultnum} km</h1>
-        <h1 className="title">Počet bodů za tohle kolo: 0</h1>
-        <h1 className="title">Celkový počet bodů: 0</h1>
+        <div>
+          <h1 className="title">Vzdálenost: {resultnum} km</h1>
+          <h1 className="title">Počet bodů za tohle kolo: 0</h1>
+          <h1 className="title">Celkový počet bodů: 0</h1>
+        </div>
       </div>
 
-    </div>      
-
-
-
-    <MapContainer center={center} zoom={2} className="leaflet-container">
+      <MapContainer center={center} zoom={2} className="leaflet-container">
         <TileLayer
           //url = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-          url='https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png'
+          url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png"
           noWrap={true}
-          attribution= 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        
+
         <MapEvents />
-        
+
         <Marker position={position} />
 
-        {result ? <Result/> : <></>}
-
-
-    </MapContainer>
-  
+        {result ? <Result /> : <></>}
+      </MapContainer>
     </div>
   );
 }
 
 export default App;
 
-      /*
+/*
       <div>
         <h1 className="title">Vzdálenost: {reskm} km</h1>
         <h1 className="title">Počet bodů za tohle kolo: 0</h1>
@@ -192,7 +200,7 @@ export default App;
       </div>
       */
 
-      /*
+/*
             <div>
       {animals.map(animal => (
         <div key={animal.name}>
